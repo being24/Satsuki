307b9cb792efbcba5f45900147f09844b770a5ca
being241 <forprom.sn.16@gmail.com>
2019-10-16 23:04:05 +0900

    urlとdiceコマンドに対応

--- cogs/common_cog.py
+++ cogs/common_cog.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
+import random
 import typing
 from datetime import datetime, timedelta
 
@@ -21,6 +22,7 @@ class Tachibana_Com(commands.Cog):  # コグとして用いるクラスを定義
 
     def __init__(self, bot):  # TestCogクラスのコンストラクタ。Botを受取り、インスタンス変数として保持。
         self.bot = bot
+        self.SCP_JP = "http://ja.scp-wiki.net"
 
     @commands.command()  # コマンドの作成。コマンドはcommandデコレータで必ず修飾する。
     async def ping(self, ctx):
@@ -30,30 +32,30 @@ class Tachibana_Com(commands.Cog):  # コグとして用いるクラスを定義
     async def draft(self, ctx):
         target_url = 'http://njr-sys.net/irc/draftReserve/'
 
-        # d_today = (datetime.now() + timedelta(hours=-3)).date()
-        d_today = '2019-10-12'
+        d_today = (datetime.now() + timedelta(hours=-3)).date()
+        # d_today = '2019-10-12'
         response = requests.get(target_url + str(d_today))
 
         soup = BeautifulSoup(response.text, 'lxml')
 
         limen_object = datetime.strptime(
             f'{d_today}-20:45:00', '%Y-%m-%d-%H:%M:%S')
-        print(limen_object)
 
         url = []
         time = []
         title = []
         author = []
 
-        for i, detail in enumerate(soup.find_all(class_='irc-table__message')):            
+        for i, detail in enumerate(soup.find_all(class_='irc-table__message')):
             if i % 4 == 0:
                 time_object = datetime.strptime(
-                    f'{d_today}-{detail.string.replace(" ", "")}', '%Y-%m-%d-%H:%M:%S')
+                    f'{d_today}-{detail.string.replace(" ", "")}',
+                    '%Y-%m-%d-%H:%M:%S')
                 if time_object < limen_object:
                     flag = 1
                 else:
                     flag = 0
-                    time.append(detail.string.replace(" ", ""))                
+                    time.append(detail.string.replace(" ", ""))
             elif i % 4 == 1:
                 if flag == 0:
                     author.append(detail.string.replace(" ", ""))
@@ -85,6 +87,45 @@ class Tachibana_Com(commands.Cog):  # コグとして用いるクラスを定義
     @draft.error
     async def draft_error(self, ctx, error):
         await ctx.send(f'to <@277825292536512513> at draft command\n{error}')
+
+    @commands.command()
+    async def url(self, ctx, call):
+        call = call.replace(" ", "").replace("　", "")
+        if "http" in call:
+            reply = "外部サイトを貼らないでください." + ctx.author.mention
+        elif "/" in call[0:1]:
+            reply = self.SCP_JP + call
+        else:
+            reply = self.SCP_JP + "/" + call
+
+        if reply is not None:
+            await ctx.send(reply)
+
+    @url.error
+    async def url_error(self, ctx, error):
+        await ctx.send(f'to <@277825292536512513> at url command\n{error}')
+
+    @commands.command()
+    async def dice(self, ctx, num1: int, num2: typing.Optional[int] = 0):
+        nums = [num1, num2]
+
+        nums.sort()
+
+        if int(nums[1]) > 10000:
+            await ctx.send("入力値が大きすぎです")
+
+        else:
+            x = random.randint(nums[0], nums[1])
+            if x is not None:
+                await ctx.send("出目は " + str(x) + " です")
+
+    @dice.error
+    async def dice_error(self, ctx, error):
+        if discord.ext.commands.errors.BadArgument:
+            await ctx.send('入力値が不正です')
+        else:
+            print(type(error))
+            await ctx.send(f'to <@277825292536512513> at dice command\n{error}')
 
     # エラーキャッチ
     @commands.Cog.listener()
