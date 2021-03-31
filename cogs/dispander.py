b3f9c0173d74460256cc0bb33e7fc3b4b01ae410
being <34680324+being24@users.noreply.github.com>
2021-03-31 20:26:56 +0900

    サーバーごとに設定を変えられるように変更

--- cogs/dispander.py
+++ cogs/dispander.py
@@ -6,11 +6,13 @@ Copyright (c) 2020 1ntegrale9
 This software is released under the MIT License.
 http://opensource.org/licenses/mit-license.php
 """
-
+import re
 
 from discord import Embed
 from discord.ext import commands
-import re
+
+from cogs.utils.setting_manager import SettingManager
+from cogs.utils.common import CommonUtil
 
 regex_discord_message_url = (
     'https://(ptb.|canary.)?discord(app)?.com/channels/'
@@ -21,6 +23,8 @@ regex_discord_message_url = (
 class ExpandDiscordMessageUrl(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.setting_mng = SettingManager()
+        self.c = CommonUtil()
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -42,10 +46,23 @@ class ExpandDiscordMessageUrl(commands.Cog):
     async def extract_messsages(self, message):
         messages = []
         for ids in re.finditer(regex_discord_message_url, message.content):
-            msg_guild = self.bot.get_guild(int(ids['guild']))
+            # 同一ならTrue、片方がFalseならFalse
+            # ここで判定入れる
+            url_guild_id = int(ids['guild'])
+            msg_guild = self.bot.get_guild(url_guild_id)
             if msg_guild is None:
-                await message.channel.send("サーバに入室していません")
+                msg = await message.channel.send("サーバに入室していません")
+                await self.c.autodel_msg(msg=msg)
                 return []
+
+            if message.guild.id == url_guild_id:  # 同一サーバー内ならOK
+                pass
+            else:  # 同一サーバー内でなくて、どちらかがFalseならFalse(NAND)
+                if not (await self.setting_mng.is_dispand(message.guild.id) and await self.setting_mng.is_dispand(url_guild_id)):
+                    msg = await message.channel.send("送信が許可されていません")
+                    await self.c.autodel_msg(msg=msg)
+                    return []
+
             fetched_message = await self.fetch_message_from_id(
                 guild=msg_guild,
                 channel_id=int(ids['channel']),
@@ -85,6 +102,23 @@ class ExpandDiscordMessageUrl(commands.Cog):
                 url=message.attachments[0].proxy_url
             )
         return embed
+
+    @commands.command(description='dispandの状態確認関数')
+    @commands.has_permissions(manage_guild=True)
+    async def show_dispand(self, ctx):
+        """サーバー間でのメッセージ展開が有効かどうかを確認する関数。サーバー管理権限が必要"""
+        footer_word = '\nサーバー間展開を使用するためには、双方のサーバーが展開を許可している必要があります'
+        if await self.setting_mng.is_dispand(ctx.guild.id):
+            await ctx.reply(f'本サーバーのメッセージ展開は許可されています{footer_word}')
+        else:
+            await ctx.reply(f'本サーバーのメッセージ展開は許可されていません{footer_word}')
+
+    @commands.command(description='dispandを制御する関数')
+    @commands.has_permissions(manage_guild=True)
+    async def set_dispand(self, ctx, tf: bool):
+        """サーバー間でのメッセージ展開を制御する関数。サーバー管理権限が必要"""
+        await self.setting_mng.set_dispand(ctx.guild.id, tf)
+        await ctx.reply(f'メッセージ展開設定を{tf}に設定しました')
 
 
 def setup(bot):
