46a162fab72da5461d0dfbea355044a8cf7fab5b
being <34680324+being24@users.noreply.github.com>
2021-05-23 20:19:23 +0900

    init

---
+++ cogs/prop_cog.py
@@ -0,0 +1,124 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from typing import List, Union
+
+import discord
+from discord.ext import commands
+from discord.ext.menus import ListPageSource, MenuPages
+
+from .utils.article_manager import ArticleManager, SCPArticleDatacls
+from .utils.common import CommonUtil
+
+
+class ProposalPager(ListPageSource):
+    def __init__(self, ctx, data):
+        self.ctx = ctx
+        super().__init__(data, per_page=10)
+        self.root_url = 'http://scp-jp.wikidot.com/'
+
+    async def write_page(self, menu, fields: List[SCPArticleDatacls] = []) -> discord.Embed:
+        offset = (menu.current_page * self.per_page) + 1
+        len_data = len(self.entries)
+
+        embed = discord.Embed(
+            title="該当する提言は以下の通りです",
+            description=f"{len_data}件ヒット",
+            color=discord.Color.darker_grey())
+
+        embed.set_footer(
+            text=f"{offset:,} - {min(len_data, offset+self.per_page-1):,} of {len_data:,} records.")
+
+        for data in fields:
+            embed.add_field(
+                name=data.title,
+                value=f'{self.root_url}{data.fullname}\nAuthor : {data.created_by}',
+                inline=False)
+
+        return embed
+
+    async def format_page(self, menu, entries):
+
+        return await self.write_page(menu, entries)
+
+
+class ProposalArticleCog(commands.Cog, name='提言コマンド'):
+    def __init__(self, bot):
+        self.bot = bot
+        self.article_mng = ArticleManager()
+        self.c = CommonUtil()
+
+    async def start_paginating(self, ctx, data_list: List[SCPArticleDatacls]):
+        menu = MenuPages(source=ProposalPager(ctx, data_list),
+                         delete_message_after=False,
+                         clear_reactions_after=True,
+                         timeout=60.0)
+        await menu.start(ctx)
+
+    async def send_message(self, ctx, data_list: Union[List[SCPArticleDatacls], None]) -> None:
+        if data_list is None:
+            msg = await ctx.reply("該当する提言は見つかりませんでした")
+            await self.c.autodel_msg(msg=msg)
+            return
+
+        elif len(data_list) == 1:
+            data = data_list[0]
+            await ctx.send(f'{data.title}\n{self.bot.root_url}{data.fullname}')
+            return
+
+        else:
+            await self.start_paginating(ctx, data_list)
+            return
+
+    @commands.group(invoke_without_command=True, description='提言記事を検索するコマンド',aliases=['prop'])
+    async def proposal(self, ctx, word: str):
+        """引数から提言を検索するコマンド"""
+        if ctx.invoked_subcommand is None:
+            data_list = await self.article_mng.get_data_from_all_and_tag(all_=word, tags=['001提言'])
+
+            await self.send_message(ctx, data_list)
+    # サブコマンドでtitile URL -d を作るべし
+
+    @proposal.command(description='提言記事をurlから検索するコマンド', aliases=['-u'])
+    async def url_(self, ctx, url: str):
+        """urlから提言を検索するコマンド"""
+
+        data_list = await self.article_mng.get_data_from_url_and_tag(url=url, tags=['001提言'])
+        await self.send_message(ctx, data_list)
+
+    @proposal.command(description='提言記事を著者から検索するコマンド', aliases=['-a'])
+    async def author(self, ctx, author: str):
+        """著者から提言を検索するコマンド"""
+
+        data_list = await self.article_mng.get_data_from_author_and_tag(author=author, tags=['001提言'])
+        await self.send_message(ctx, data_list)
+
+    @proposal.command(description='提言記事をタイトルから検索するコマンド', aliases=['-t'])
+    async def title(self, ctx, title: str):
+        """タイトルから提言を検索するコマンド"""
+
+        data_list = await self.article_mng.get_data_from_title_and_tag(title=title, tags=['001提言'])
+        await self.send_message(ctx, data_list)
+
+    @proposal.command(description='提言記事の詳細版を作成するコマンド', aliases=['-d'])
+    async def detail(self, ctx, all_: str):
+        """提言の詳細版を検索するコマンド\n複数ヒットした場合は通常の一覧表示を行います"""
+
+        data_list = await self.article_mng.get_data_from_all_and_tag(all_=all_, tags=['001提言'])
+        if data_list is None:
+            msg = await ctx.reply("該当する提言は見つかりませんでした")
+            await self.c.autodel_msg(msg=msg)
+
+        elif len(data_list) > 1:
+            await self.send_message(ctx, data_list)
+            msg = await ctx.reply('複数ヒットしたため、一覧を表示します')
+            await self.c.autodel_msg(msg=msg)
+
+        else:
+            embed = self.c.create_detail_embed(data_list[0])
+            await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(ProposalArticleCog(bot))
