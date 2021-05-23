0673b321e8de110a3e3dd535ba9f7a2a97d817a5
being <34680324+being24@users.noreply.github.com>
2021-05-23 19:11:31 +0900

    ♻️ taleを分離

--- cogs/scp_cog.py
+++ cogs/scp_cog.py
@@ -75,40 +75,7 @@ class SCPArticleCog(commands.Cog, name='SCPコマンド'):
 
         return data
 
-    async def create_detail_embed(self, data: SCPArticleDatacls) -> discord.Embed:
-        """詳細をembedにする関数、tzとか文字数制限とかはよしなにしてくれる
-
-        Args:
-            data (SCPArticleDatacls): 記事データ
-
-        Returns:
-            discord.Embed: 色々調整したデータ
-        """
-        tags = (' ').join(data.tags)
-        created_at = pytz.utc.localize(data.created_at)
-        created_at_jst = created_at.astimezone(
-            pytz.timezone(self.local_timezone.zone))
-        if len(data.metatitle) > 250:
-            metatitle = f"{data.metatitle[:250]}{'...'}"
-        else:
-            metatitle = data.metatitle
-        embed = discord.Embed(
-            title=f"{metatitle}",
-            url=f"{self.root_url}{data.fullname}",
-            color=0x8f1919)
-        embed.add_field(
-            name="created_by",
-            value=f"{data.created_by}",
-            inline=False)
-        embed.add_field(
-            name="created_at",
-            value=f"{created_at_jst.strftime('%Y/%m/%d %H:%M')}",
-            inline=False)
-        embed.add_field(name="rate", value=f"{data.rating}", inline=True)
-        embed.add_field(name="tags", value=f"{tags}", inline=True)
-
-        return embed
-
+ 
     @commands.group(invoke_without_command=True, description='SCP記事を検索するコマンド')
     async def scp(self, ctx, num_brt: str):
         """引数からscpを検索するコマンド-シンプル版"""
@@ -149,53 +116,6 @@ class SCPArticleCog(commands.Cog, name='SCPコマンド'):
             await ctx.send(embed=embed)
 
         return
-
-    @commands.group(description='TALE記事を検索するコマンド')
-    async def tale(self, ctx, word: str):
-        """引数からtaleを検索するコマンド"""
-
-        if ctx.invoked_subcommand is None:
-            data_list = await self.article_mng.get_data_from_all_and_tag(all=word, tags=['tale'])
-
-            if data_list is None:
-                msg = await ctx.reply("該当するtaleは見つかりませんでした")
-                await self.c.autodel_msg(msg=msg)
-                return
-
-            if (len_of_data := len(data_list)) > 30:
-                msg = await ctx.reply(f"ヒット{len_of_data}件、多すぎます")
-                await self.c.autodel_msg(msg=msg)
-                return
-
-            elif len_of_data == 1:
-                data = data_list[0]
-                await ctx.send(f'{data.title}\n{self.root_url}{data.fullname}')
-                return
-
-            else:
-                embed = discord.Embed(
-                    title="TALE検索結果",
-                    description=f"検索ワード'{word}'にヒットしたTaleは{len_of_data}件です",
-                    color=0xff8000)
-
-                for data in data_list:
-                    embed.add_field(
-                        name=data.title,
-                        value=f'{self.root_url}{data.fullname}\nAuthor : {data.created_by}',
-                        inline=False)
-
-                embed.set_footer(text="タイトル、URL、著者名から検索しています")
-                await ctx.send(embed=embed)
-                return
-
-    # サブコマンドでtitile 著者 URL -d を作るべし
-
-    """
-    複数→従来通り
-    一個→シンプル
-    -dオプション→複数個あったら提示して一個に絞って詳細版
-                 一個ならそのまま詳細版
-    """
 
 
 def setup(bot):
