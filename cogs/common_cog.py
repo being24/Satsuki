c8864e05552452d11d93f8ccaef84b97a77039ce
being <34680324+being24@users.noreply.github.com>
2021-05-26 21:44:05 +0900

    ⚡️ 30日以上未来の通知は除去する

--- cogs/common_cog.py
+++ cogs/common_cog.py
@@ -263,7 +263,10 @@ class SatsukiCom(commands.Cog, name='皐月分類外コマンド'):
             dict_time_m5 = datetime.strptime(
                 self.timer_dict[key]['-5'], '%Y-%m-%d %H:%M:%S')
 
-            if (now - dict_time_just).days > 1:
+            if (days := (now - dict_time_just).days) > 1:
+                del_list.append(key)
+
+            elif days < - 30:
                 del_list.append(key)
 
             elif dict_time_just < now:
