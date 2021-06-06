43b348dd41958ed01143e2735263b21a38636efc
being <34680324+being24@users.noreply.github.com>
2021-06-06 16:19:07 +0900

    🔨 定例会コマンドをDB化、独自運用も可能に

--- cogs/critique_meeting.py
+++ cogs/critique_meeting.py
@@ -4,24 +4,24 @@
 
 import asyncio
 import html
-import os
 import random
 import re
-import typing
 from dataclasses import dataclass
 from datetime import datetime, timedelta
 from typing import List
 
+import aiohttp
 import discord
 import feedparser
 import html2text
-import requests
+import pytz
+import tzlocal
 from bs4 import BeautifulSoup
 from discord.ext import commands
 from more_itertools import divide
 
 from cogs.utils.common import CommonUtil
-from cogs.utils.meeting_manager import ReserveData, MeetingManager
+from cogs.utils.meeting_manager import MeetingManager, ReserveData
 
 
 @dataclass
@@ -33,78 +33,50 @@ class RssData:
     content: str
 
 
-'''
-def shape_content_to_discord(content: str) -> str:
-    content = content.replace("<p>", "\n")
-    content = content.replace("</p>", "\n")
-    content = content.replace("</div>", "")
-
-    content = content.replace("<br />", "\n")
-
-    if '<span class="rt">' in content:  # ルビ
-        siz0_0 = content.find('<span class="rt">')
-        siz0_1 = content.find('</span>', siz0_0 + 1)
-        content = content.replace('<span class="rt">', " - [")
-        content = content.replace('</span></span>', "]")  # 多分ここ違う
-
-    if '<strong>' in content:  # 強調
-        content = content.replace('<strong>', "**")
-        content = content.replace('</strong>', "**")
-
-    if '<span style="text-decoration: content-through;">' in content:  # 取り消し
-        content = content.replace(
-            '<span style="text-decoration: content-through;">', "~~")
-        content = content.replace('</span>', "~~ ", 1)
-
-    if '<span style="text-decoration: undercontent;">' in content:  # 下線
-        content = content.replace(
-            '<span style="text-decoration: undercontent;">', "__")
-        content = content.replace('</span>', "__ ", 1)
-
-    if '<em>' in content:  # 斜体
-        content = content.replace('<em>', "*")
-        content = content.replace('</em>', "*")
-
-    if 'href' in content:
-        match = re.findall(r'href=[\'"]?([^\'" >]+)', content)
-        if len(match) == 0:
-            content = ""
-        elif "javascript" in match[0]:
-            content = ""
-
-        if len(match) != 0:
-            deco_f = ' ['
-            deco_b = f"]({match[0]}) "
-
-        p = re.compile(r"<[^>]*?>")
-        content = p.sub(f"{deco_b}", content)
-        content = content.replace(deco_b, deco_f, 1)
-
-    content = html.unescape(content)
-    # result.append(content)
-
-    return content
-'''
-
-
 class CritiqueCog(commands.Cog, name='批評定例会用コマンド'):
     def __init__(self, bot):
         self.bot = bot
         self.SCP_JP = "http://scp-jp.wikidot.com"
-        self.root_path = os.path.dirname(
-            os.path.dirname(os.path.abspath(__file__)))
+
+        self.local_timezone = tzlocal.get_localzone()
+
         self.c = CommonUtil()
         self.meeting_mng = MeetingManager()
 
     @staticmethod
-    def from_struct_time_to_datetime(struct_time: str):
+    def from_struct_time_to_datetime(struct_time: str) -> datetime:
+        """wikidot-RSSの文字列をdatetimeに変換する
+
+        Args:
+            struct_time (str): RSSの文字列
+
+        Returns:
+            datetime: awareなdatetime
+        """
         postdate = datetime.strptime(struct_time, "%a, %d %b %Y %H:%M:%S %z")
         postdate = postdate.astimezone()
         return postdate
 
-    def get_content_from_rss(self, target: str) -> List[RssData]:
-        feed_data = feedparser.parse(target)
+    async def get_content_from_rss(self, target: str) -> List[RssData]:
+        """RSSのデータをパース、RssDataに変換して返す関数
+
+        Args:
+            target (str): RSSのデータ
+
+        Returns:
+            List[RssData]: RSSのデータクラス
+        """
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(target) as r:
+                if r.status == 200:
+                    response = await r.text()
+                else:
+                    raise
+
+        feed_data = feedparser.parse(response)
         scp_rss: List[RssData] = []
+
         for entry in feed_data.entries:
             content = html2text.html2text(str(entry["content"][0]['value']))
             content = content.replace(')', ') ')
@@ -116,28 +88,32 @@ class CritiqueCog(commands.Cog, name='批評定例会用コマンド'):
                 author=str(entry['wikidot_authorname']),
                 content=content)
             scp_rss.append(data)
+
         return scp_rss
 
-    @ commands.command(aliases=['df'], description='本日の下書き予約を表示します')
-    @ commands.has_permissions(kick_members=True)
-    async def draft(self, ctx, num: typing.Optional[int] = 0):
-        """本日の下書き予約を表示します.引数に数字を与えるとその下書き予約を表示します."""
-        target_url = 'http://njr-sys.net/irc/draftReserve/'
+    def generate_ReserveData(
+            self,
+            d_today: str,
+            response: str) -> List[ReserveData]:
+        """取得したソースからReserveDataのリストを生成する関数
 
-        d_today = (datetime.now() + timedelta(hours=-3)).date()
-        # d_today = '2019-10-12'
-        response = requests.get(target_url + str(d_today))
+        Args:
+            d_today (str): 今日の日付（3hずれてる）
+            response (str): ソース
 
-        soup = BeautifulSoup(response.text, "html.parser")
+        Returns:
+            List[ReserveData]: ReserveDataのデータクラス
+        """
 
-        limen_time = datetime.strptime(
-            f'{d_today}-20:45:00', '%Y-%m-%d-%H:%M:%S')
+        soup = BeautifulSoup(response, "html.parser")
 
         table = soup.find(class_='irc-table zebra-table')
         reserve_list = table.find_all('tr')
+
         zero_time = datetime.fromtimestamp(0)
 
         ReserveData_list: List[ReserveData] = []
+
         for elements in reserve_list:
             author_name = 'None'
             draft_title = 'None'
@@ -157,6 +133,10 @@ class CritiqueCog(commands.Cog, name='批評定例会用コマンド'):
                             element.string):
                         reserve_time = datetime.strptime(
                             f'{d_today}-{element.string.replace(" ", "")}', '%Y-%m-%d-%H:%M:%S')
+                        reserve_time = reserve_time.astimezone(
+                            self.local_timezone)
+                        reserve_time = reserve_time.astimezone(pytz.utc)
+
                     else:
                         author_name = element.string
                         if author_name is None:
@@ -166,42 +146,58 @@ class CritiqueCog(commands.Cog, name='批評定例会用コマンド'):
                     if draft_title is None:
                         draft_title = 'None'
 
-            if reserve_time >= limen_time:
-                data = ReserveData(
-                    author_name=author_name[:50],
-                    draft_title=draft_title[:50],
-                    draft_link=draft_link,
-                    reserve_time=reserve_time)
-                ReserveData_list.append(data)
+            data = ReserveData(
+                msg_id=0,
+                author_name=author_name[:50],
+                draft_title=draft_title[:50],
+                draft_link=draft_link,
+                reserve_time=reserve_time)
+            ReserveData_list.append(data)
+        return ReserveData_list
 
-        if len(ReserveData_list) == 0:
-            await ctx.send("本日の予約はありません")
-            return
+    async def send_reserve_embed(self, ctx, num: int, list_reservation_available: List[ReserveData]) -> None:
+        """予約リストをnumに合わせてembedで送ってくれるやつ
+
+        Args:
+            ctx ([type]): いつもの
+            num (int): 0なら全部、それ以外なら一つ
+            list_reservation_available (List[ReserveData]): 対象のリスト
+        """
 
         title_message = "下書き批評予約システムからデータを取得します"
 
         if num == 0:
             embed = discord.Embed(
                 title=title_message,
-                description=f"本日の下書き批評予約は全{len(ReserveData_list)}件です",
+                description=f"本日の下書き批評予約は全{len(list_reservation_available)}件です",
                 color=0xff0080)
 
-            for num, data in enumerate(ReserveData_list):
+            for num, data in enumerate(list_reservation_available):
+                reserve_time_jst = self.c.convert_utc_into_jst(
+                    data.reserve_time)
+
+                reserve_time_jst = reserve_time_jst.strftime(
+                    '%Y-%m-%d %H:%M:%S')
                 embed.add_field(
                     name=data.author_name,
-                    value=f'{num+1} : [{data.draft_title}]({data.draft_link})\tat: {data.reserve_time}',
+                    value=f'{num+1} : [{data.draft_title}]({data.draft_link})\tat: {reserve_time_jst}',
                     inline=False)
 
             embed.set_footer(text="受付時間外の予約は無効です!")
             await ctx.send(embed=embed)
 
         else:
+            if list_len := len(list_reservation_available) < num:
+                msg = await ctx.reply('指定された数字が大きすぎます')
+                await self.c.autodel_msg(msg=msg)
+                return
+
             embed = discord.Embed(
                 title=title_message,
-                description=f"{num}/{len(ReserveData_list)}件目の下書きです",
+                description=f"{num}/{list_len}件目の下書きです",
                 color=0xff0080)
             num = num - 1
-            data = ReserveData_list[num]
+            data = list_reservation_available[num]
             embed.add_field(
                 name=data.author_name,
                 value=f'{num+1} : [{data.draft_title}]({data.draft_link})\tat: {data.reserve_time}',
@@ -210,10 +206,155 @@ class CritiqueCog(commands.Cog, name='批評定例会用コマンド'):
             embed.set_footer(text="受付時間外の予約は無効です!")
             await ctx.send(embed=embed)
 
-    @ commands.command(aliases=['mt'])
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.meeting_mng.create_table()
+
+    @ commands.group(invoke_without_command=True,
+                     aliases=['df'], description='本日の下書き予約を表示します')
+    @ commands.has_permissions(kick_members=True)
+    async def draft(self, ctx, num: int = 0):
+        """本日の下書き予約を表示します.引数に数字を与えるとその下書き予約を表示します."""
+
+        utc_time = datetime.utcnow()
+        utc_time = utc_time.astimezone(pytz.utc)
+
+        jst_time = datetime.utcnow().astimezone(self.local_timezone)
+
+        d_today = (jst_time + timedelta(hours=-3)
+                   ).strftime('%Y-%m-%d')  # 日付をまたぐときがあるのでその対策
+
+        """
+        d_today = '2019-10-12'  # debug
+        jst_time = datetime.strptime(
+            f'{d_today}-20:46:00', '%Y-%m-%d-%H:%M:%S')
+        jst_time = jst_time.astimezone(self.local_timezone)
+        """
+
+        limen_time = datetime.strptime(
+            f'{d_today}-20:45:00', '%Y-%m-%d-%H:%M:%S')
+        limen_time = self.local_timezone.localize(limen_time)
+
+        if num == 0:
+            target_url = 'http://njr-sys.net/irc/draftReserve/'
+
+            async with aiohttp.ClientSession() as session:
+                async with session.get(f'{target_url}{d_today}') as r:
+                    if r.status == 200:
+                        response = await r.text()
+                    else:
+                        await ctx.send("データ取得に失敗しました")
+                        return
+
+            ReserveData_list = self.generate_ReserveData(d_today, response)
+
+            full_reserve_data = await self.meeting_mng.upsert_and_return_reserve_data(jst_time, ReserveData_list)
+
+        else:
+            full_reserve_data = await self.meeting_mng.get_reserve_data_from_date(jst_time)
+
+        list_reservation_available = []
+        for data in full_reserve_data:
+            reserve_time = self.c.convert_utc_into_jst(data.reserve_time)
+
+            if reserve_time >= limen_time:
+                list_reservation_available.append(data)
+
+        if len(list_reservation_available) == 0:
+            await ctx.send("本日の予約はありません")
+            return
+
+        await self.send_reserve_embed(ctx, num, list_reservation_available)
+
+    @draft.command(description='すべての今日の予約を表示するコマンド', aliases=['-a'])
+    async def all(self, ctx, num: int = 0):
+        """対象外であっても表示します、一応作成"""
+        utc_time = datetime.utcnow()
+        utc_time = utc_time.astimezone(pytz.utc)
+
+        jst_time = datetime.utcnow().astimezone(self.local_timezone)
+
+        d_today = (jst_time + timedelta(hours=-3)
+                   ).strftime('%Y-%m-%d')   # 日付をまたぐときがあるのでその対策
+
+        """
+        d_today = '2019-10-12'  # debug
+        jst_time = datetime.strptime(
+            f'{d_today}-20:45:00', '%Y-%m-%d-%H:%M:%S')
+        jst_time = jst_time.astimezone(self.local_timezone)
+        """
+
+        if num == 0:
+            target_url = 'http://njr-sys.net/irc/draftReserve/'
+
+            async with aiohttp.ClientSession() as session:
+                async with session.get(f'{target_url}{d_today}') as r:
+                    if r.status == 200:
+                        response = await r.text()
+                    else:
+                        await ctx.send("データ取得に失敗しました")
+                        return
+
+            ReserveData_list = self.generate_ReserveData(d_today, response)
+
+            full_reserve_data = await self.meeting_mng.upsert_and_return_reserve_data(jst_time, ReserveData_list)
+
+        else:
+            full_reserve_data = await self.meeting_mng.get_reserve_data_from_date(jst_time)
+
+        if full_reserve_data is None:
+            await ctx.send("本日の予約はありません")
+            return
+
+        await self.send_reserve_embed(ctx, num, full_reserve_data)
+
+    @ commands.command(aliases=['res'],
+                       description="定例会の予約をするコマンド",
+                       hidden=True)
+    # @ commands.dm_only()
+    async def reserve(self, ctx, title: str, url: str):
+        """煮汁の不具合に備えて実装"""
+        time = datetime.utcnow()
+        time_jst = self.c.convert_utc_into_jst(time)
+
+        match_url = re.match(
+            r"https?://scp-jp-sandbox3\.wikidot\.com/[\w/:%#\$&\?\(\)~\.=\+\-]+", url)
+        if not match_url:
+            raise commands.BadArgument
+
+        name = ctx.message.author.nick
+        if name is None:
+            name = ctx.message.author.name
+
+        data = ReserveData(
+            msg_id=ctx.message.id,
+            author_name=name[:50],
+            draft_title=title[:50],
+            draft_link=url[:100],
+            reserve_time=ctx.message.created_at)
+
+        await self.meeting_mng.add_reserve_data(data)
+        time_jst = self.c.convert_utc_into_jst(ctx.message.created_at)
+        time_jst = time_jst.strftime('%Y-%m-%d %H:%M:%S')
+        await ctx.reply(f'{time_jst}に{data.draft_title}ついての予約を受け付けました')
+
+    @ reserve.error
+    async def reserve_error(self, ctx, error):
+        if isinstance(error, commands.CheckFailure):
+            msg = await ctx.reply('このコマンドはDMで実行してください')
+            await self.c.autodel_msg(msg)
+        elif isinstance(error, commands.MissingRequiredArgument):
+            msg = await ctx.reply('引数が足りません')
+            await self.c.autodel_msg(msg)
+        elif isinstance(error, commands.BadArgument):
+            msg = await ctx.reply('第二引数に不正な文字列が入力されています、urlをそのまま張り付けてください')
+            await self.c.autodel_msg(msg)
+
+    @ commands.command(aliases=['mt'], description="定例会告知用コマンド")
     @ commands.has_permissions(kick_members=True)
     async def meeting(self, ctx):
-        contentlist = self.get_content_from_rss(self.bot.meeting_addr)
+        """定例会告知スレッドRSSから書き込みを取得、送信するコマンド"""
+        contentlist = await self.get_content_from_rss(self.bot.meeting_addr)
         data = contentlist[0]
 
         embed = discord.Embed(
@@ -238,36 +379,10 @@ class CritiqueCog(commands.Cog, name='批評定例会用コマンド'):
 
         await ctx.send(embed=embed)
 
-    @ commands.command(aliases=['res'])  # 設定でdfの先を変更できるようにする
-    # @ commands.dm_only()
-    async def reserve(self, ctx, title: str, url: str):
-        if not 'Saturday' == datetime.today().strftime('%A'):
-            msg = await ctx.reply('今日は土曜日では無いため、予約を受け付けません')
-            await self.c.autodel_msg(msg)
-            return
-
-        match_url = re.match(
-            r"https?://scp-jp-sandbox3\.wikidot\.com/[\w/:%#\$&\?\(\)~\.=\+\-]+", url)
-        if not match_url:
-            raise commands.BadArgument
-
-        pass
-
-    @ reserve.error
-    async def reserve_error(self, ctx, error):
-        if isinstance(error, commands.CheckFailure):
-            msg = await ctx.reply('このコマンドはDMで実行してください')
-            await self.c.autodel_msg(msg)
-        elif isinstance(error, commands.MissingRequiredArgument):
-            msg = await ctx.reply('引数が足りません')
-            await self.c.autodel_msg(msg)
-        elif isinstance(error, commands.BadArgument):
-            msg = await ctx.reply('第二引数に不正な文字列が入力されています、urlをそのまま張り付けてください')
-            await self.c.autodel_msg(msg)
-
-    @ commands.command(aliases=['sh'])
+    @ commands.command(aliases=['sh'],description="リアクションをつけた人を均等に分割するコマンド")
     @ commands.has_permissions(kick_members=True)
     async def shuffle(self, ctx, num: int = 2):
+        """あんまり使ってるところを見たことはない"""
         settime = 10.0 * 60
         reaction_member = []
         emoji_in = '\N{THUMBS UP SIGN}'
--- cogs/utils/meeting_manager.py
+++ cogs/utils/meeting_manager.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import asyncio
-import pathlib
 from dataclasses import dataclass
 import datetime
-from typing import List, Union
+from typing import List, Optional, Union
 
-from sqlalchemy import select, update
+from sqlalchemy import select, update, and_
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import Column
@@ -20,6 +19,7 @@ Base = declarative_base()
 
 @dataclass
 class ReserveData:
+    msg_id: int
     draft_title: str
     draft_link: str
     author_name: str
@@ -27,10 +27,10 @@ class ReserveData:
 
 
 class ReserveDataDB(Base):
-    d_today = str(datetime.date.today())
-    __tablename__ = d_today
+    __tablename__ = 'ReserveData'
 
-    msd_id = Column(BIGINT, primary_key=True)
+    id = Column(INTEGER, autoincrement=True, primary_key=True)
+    msg_id = Column(BIGINT, nullable=False)
     draft_title = Column(VARCHAR, nullable=True)
     draft_link = Column(VARCHAR, nullable=False)
     author_name = Column(VARCHAR, nullable=False)
@@ -58,6 +58,7 @@ class MeetingManager():
         """
 
         db_data_raw = ReserveData(
+            msg_id=data.msg_id,
             draft_title=data.draft_title,
             draft_link=data.draft_link,
             author_name=data.author_name,
@@ -69,20 +70,123 @@ class MeetingManager():
         """dataclassからDBのデータを作成する関数
 
         Args:
-            data (ReserveData): dataclassからReserveDataDBを返す関数
+            data (ReserveData): dataclass
 
         Returns:
             ReserveDataDB: DB型
         """
 
         new_reserve = ReserveDataDB(
-            msd_id=msg_id,
+            msg_id=msg_id,
             draft_title=data.draft_title,
             draft_link=data.draft_link,
             author_name=data.author_name,
             reserve_time=data.reserve_time)
 
         return new_reserve
+
+    async def insert_many_nijiru(self, data_list: List[ReserveData]) -> None:
+        """煮汁システムから取得した予約データを格納する関数
+
+        Args:
+            data_list (List[ReserveData]): データのリスト(msg_idを入力する欄は0)
+        """
+
+        DB_data_list = [self.return_DB_obj(0, data) for data in data_list]
+        async with AsyncSession(engine) as session:
+            async with session.begin():
+                try:
+                    session.add_all(DB_data_list)
+                except BaseException:
+                    await session.rollback()
+                    raise
+                else:
+                    await session.commit()
+
+    async def add_reserve_data(self, data: ReserveData) -> None:
+        """煮汁システムから取得した予約データを格納する関数
+
+        Args:
+            data_list (List[ReserveData]): データのリスト(msg_idを入力する欄は0)
+        """
+
+        msg_id = data.msg_id
+        db_data = self.return_DB_obj(msg_id, data)
+
+        async with AsyncSession(engine) as session:
+            async with session.begin():
+                try:
+                    session.add(db_data)
+                except BaseException:
+                    await session.rollback()
+                    raise
+                else:
+                    await session.commit()
+
+    async def get_reserve_data_from_date(self, date_jst: datetime.datetime) -> Optional[List[ReserveData]]:
+        """指定された日の予約データを取得する関数
+
+        Args:
+            date_jst (datetime.datetime): 時間（これから日を計算する）
+
+        Returns:
+            Optional[List[ReserveData]]: 予約データのリスト
+        """
+
+        first = date_jst.replace(hour=0, minute=0, second=0, microsecond=0)
+        end = date_jst.replace(hour=23, minute=59, second=59, microsecond=0)
+
+        async with AsyncSession(engine) as session:
+            async with session.begin():
+                try:
+                    stmt = select(ReserveDataDB).filter(
+                        and_(
+                            (ReserveDataDB.reserve_time >= first),
+                            (ReserveDataDB.reserve_time <= end))).order_by(
+                        ReserveDataDB.reserve_time)
+                    result = await session.execute(stmt)
+                    result = result.all()
+                    result = [
+                        self.return_dataclass(
+                            data[0]) for data in result]
+
+                except BaseException:
+                    await session.rollback()
+                    raise
+                else:
+                    await session.commit()
+
+        if len(result) == 0:
+            return None
+        else:
+            return result
+
+    async def upsert_and_return_reserve_data(self, jst_time: datetime.datetime, ReserveData_list: List[ReserveData]) -> Optional[List[ReserveData]]:
+        """nijiruのデータとDBのデータを比較して、存在しないものを追加後、今日の分のデータを返す関数
+
+        Args:
+            jst_time (datetime): ３時間ずれたdatetime
+            ReserveData_list (List[ReserveData]): RSSから取得したデータ
+
+        Returns:
+            Optional[List[ReserveData]]: 今日の予約リスト
+        """
+
+        data_from_db = await self.get_reserve_data_from_date(jst_time)
+
+        write_list = []
+        if data_from_db is not None:
+            db_url_list = [data.draft_link for data in data_from_db]
+            for data in ReserveData_list:
+                if data.draft_link not in db_url_list:
+                    write_list.append(data)
+        else:
+            write_list = ReserveData_list
+
+        await self.insert_many_nijiru(write_list)
+
+        data_from_db = await self.get_reserve_data_from_date(jst_time)
+        return data_from_db
 
 
 if __name__ == "__main__":
