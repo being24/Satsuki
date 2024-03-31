52ed5017d4c7c1ad94fd1578ffd967684627a7a8
being <34680324+being24@users.noreply.github.com>
2024-03-31 13:01:20 +0000

    count関連の追加

--- cogs/utils/ayame_client.py
+++ cogs/utils/ayame_client.py
@@ -1,9 +1,12 @@
 import asyncio
 import datetime
+import logging
 import urllib
 
 import aiohttp
 from pydantic.dataclasses import dataclass
+
+logger = logging.getLogger("discord")
 
 
 @dataclass
@@ -64,6 +67,44 @@ class AyameSearchQuery:
         return urllib.parse.urlencode(query)
 
 
+@dataclass
+class AyameSearchCountQuery:
+    title: str | None
+    tags: list[str] | None
+    author: str | None
+    rate_min: int | None
+    rate_max: int | None
+    date_from: datetime.datetime | None
+    date_to: datetime.datetime | None
+
+    def to_query(self) -> str:
+        query = []
+
+        if self.title is not None:
+            query.append(("title", self.title))
+
+        if self.tags is not None:
+            for tag in self.tags:
+                query.append(("tags", tag))
+
+        if self.author is not None:
+            query.append(("author", self.author))
+
+        if self.rate_min is not None:
+            query.append(("rate_min", str(self.rate_min)))
+
+        if self.rate_max is not None:
+            query.append(("rate_max", str(self.rate_max)))
+
+        if self.date_from is not None:
+            query.append(("date_from", self.date_from.isoformat()))
+
+        if self.date_to is not None:
+            query.append(("date_to", self.date_to.isoformat()))
+
+        return urllib.parse.urlencode(query)
+
+
 class AyameClient:
     def __init__(self) -> None:
         pass
@@ -74,6 +115,9 @@ class AyameClient:
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
                 if response.status != 200:
+                    logger.error(
+                        f"Failed to get data from Ayame API: {response.status}, {query.to_query()}"
+                    )
                     return []
 
                 data = await response.json()
@@ -84,6 +128,10 @@ class AyameClient:
             # itemにmetatitleのキーがない場合、空文字列を代入
             if "metatitle" not in item:
                 item["metatitle"] = ""
+
+            # tagsが""の場合、空リストを代入
+            if item["tags"] == "":
+                item["tags"] = []
             result = AyameSearchResult(
                 fullname=item["fullname"],
                 title=item["title"],
@@ -98,21 +146,44 @@ class AyameClient:
 
         return results
 
+    async def search_complex_count(self, query: AyameSearchCountQuery) -> int:
+        url = f"https://ayameapidev.yukkuriikouze.com/search/complex_count?{query.to_query()}"
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status != 200:
+                    return 0
+
+                return await response.json()
+
 
 if __name__ == "__main__":
     ayame = AyameClient()
 
-    query = AyameSearchQuery(
+    query = AyameSearchCountQuery(
         title="173",
-        tags=["en", "scp"],
+        tags=None,
         author=None,
         rate_min=None,
         rate_max=None,
         date_from=None,
         date_to=None,
-        page=None,
-        show=None,
+    )
+
+    results = asyncio.run(ayame.search_complex_count(query))
+    print(results)
+
+    query = AyameSearchQuery(
+        title="173",
+        tags=None,
+        author=None,
+        rate_min=None,
+        rate_max=None,
+        date_from=None,
+        date_to=None,
+        page=2,
+        show=100,
     )
 
     results = asyncio.run(ayame.search_complex(query))
-    print(results)
+    print(len(results))
