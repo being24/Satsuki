ebadcef073ab5410d849369f20e21824486c65f5
being <34680324+being24@users.noreply.github.com>
2024-03-31 13:02:05 +0000

    不要なコードを除去

--- cogs/scp_cog.py
+++ cogs/scp_cog.py
@@ -1,57 +1,22 @@
+import logging
 import re
 
 import discord
-
 import mojimoji
 from discord import app_commands
 from discord.ext import commands
-from discord.ext.menus import ListPageSource, MenuPages
 
-from .utils.ayame_client import AyameClient, AyameSearchQuery, AyameSearchResult
+from .utils.ayame_client import AyameClient, AyameSearchQuery
 from .utils.common import CommonUtil
 
 number_match = re.compile(r"(?<![0-9])\d{3,4}(?![0-9])")  # 3か4桁のものだけマッチ
 
-
-# class SearchPager(ListPageSource):
-#     def __init__(self, ctx, data):
-#         self.ctx = ctx
-#         super().__init__(data, per_page=10)
-#         self.root_url = "http://scp-jp.wikidot.com/"
-#         self.c = CommonUtil()
-
-#     async def write_page(
-#         self, menu, fields: List[SCPArticleDatacls] = []
-#     ) -> discord.Embed:
-#         offset = (menu.current_page * self.per_page) + 1
-#         len_data = len(self.entries)
-
-#         embed = discord.Embed(
-#             title="検索結果は以下の通りです",
-#             description=f"{len_data}件ヒット",
-#             color=discord.Color.darker_grey(),
-#         )
-
-#         embed.set_footer(
-#             text=f"{offset:,} - {min(len_data, offset+self.per_page-1):,} of {len_data:,} records."
-#         )
-
-#         for data in fields:
-#             embed.add_field(
-#                 name=self.c.select_title(data),
-#                 value=f"{self.root_url}{data.fullname}\nAuthor : {data.created_by}",
-#                 inline=False,
-#             )
-
-#         return embed
-
-#     async def format_page(self, menu, entries):
-#         return await self.write_page(menu, entries)
+logger = logging.getLogger("discord")
 
 
 class SCPArticleCog(commands.Cog, name="SCPコマンド"):
     def __init__(self, bot):
-        self.bot: commands.bot = bot
+        self.bot: commands.Bot = bot
 
         self.ayame = AyameClient()
         self.c = CommonUtil()
@@ -82,14 +47,14 @@ class SCPArticleCog(commands.Cog, name="SCPコマンド"):
     async def scp(
         self, interaction: discord.Interaction, num_brt: str, detail: bool = True
     ):
-        await interaction.response.defer()
-
         number, brt = self.process_arg(num_brt)
         if (number and brt) is None:
             await interaction.response.send_message(
                 "入力が正しくありません", ephemeral=True
             )
             return
+
+        await interaction.response.defer()
 
         query = AyameSearchQuery(
             title=number,
@@ -113,7 +78,7 @@ class SCPArticleCog(commands.Cog, name="SCPコマンド"):
         else:
             data = result[0]
 
-        if data is None:
+        if len(result) == 0 or not data:
             await interaction.response.send_message(
                 "該当する記事が見つかりません", ephemeral=True
             )
